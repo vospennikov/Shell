0c0a270b39db14935b8ad35fd928db1ed35d2133
VOSPENNIKOV Mikhail <mikhail.vospennikov@raiffeisen.ru>
2023-05-11 12:18:29 +0400

    Add unit tests

--- Tests/ShellTests/ShellTests.swift
+++ Tests/ShellTests/ShellTests.swift
@@ -2,11 +2,28 @@ import XCTest
 @testable import Shell
 
 final class ShellTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documenation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    let shell: Shell = .live
+    
+    func test_withoutArguments() throws {
+        let uptime = try shell(.plainText("uptime"))
+        XCTAssertTrue(uptime.contains("load average"))
+    }
+    
+    func test_withInlineArguments() throws {
+        let echo = try shell(.plainText("echo 'Hello world'"))
+        XCTAssertEqual(echo, "Hello world")
+    }
+    
+    func test_throwingError() {
+        do {
+            try shell(.plainText("cd /not_exist"))
+            XCTFail("Expected expression to throw")
+        } catch let error as Shell.Error {
+            XCTAssertTrue(error.description.contains("no such file or directory"))
+            XCTAssertTrue(error.stdOut.isEmpty)
+            XCTAssertTrue(error.terminationStatus != 0)
+        } catch {
+            XCTFail("Invalid error type: \(error)")
+        }
     }
 }
